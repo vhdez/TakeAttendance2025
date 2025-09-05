ad7f4ad46b38f69ccb7d0148a6164090dc00b205
adilk <adilkabir738@gmail.com>
2025-09-05 10:09:34 -0400

    Adil attendance

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,8 @@ public class Main {
 
         System.out.println("Good morning!");
         System.out.println("Let's take attendance...");
+
         System.out.println("Minghao is maybe present!");
-
-
         System.out.println("Alex is present");
         System.out.println("Mr. Hernandez is REALLY present!");
         System.out.println("Tai is present");
