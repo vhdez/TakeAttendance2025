538e22930fd7e815c2c86b5a4ba0218e9460d0df
MinghaoZhang <mzhang26@scienceleadership.org>
2025-09-05 10:08:27 -0400

    e

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
 
         System.out.println("Good morning!");
         System.out.println("Let's take attendance...");
+        System.out.println("Minghao is maybe present!");
+
+
         System.out.println("Alex is present");
         System.out.println("Mr. Hernandez is REALLY present!");
 
