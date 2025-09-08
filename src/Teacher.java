ede752d7fc27b9e9f9d43db64a1a5b38eaedca4c
vhdez <vhernandez@scienceleadership.org>
2025-09-08 13:23:28 -0400

    Create mrH Object of type Teacher.

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,13 +1,14 @@
 public class Teacher {
     String name;
     boolean present;
+    int password;
 
     void greeting() {
         System.out.println("Hello! " + name + " is here!");
     }
 
     void status() {
-        System.out.println("Student: " + name + "th\nPresent: " + present);
+        System.out.println("Teacher: " + name + " is Present: " + present);
     }
 
 }
