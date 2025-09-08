db7f6419d69fb076952fc3ff0fef57c0377531b9
amaneasterling <aeasterling26@scienceleadership.org>
2025-09-08 13:38:10 -0400

    Finally Aman can push!!!

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
         System.out.println("Good morning!");
         System.out.println("Let's take attendance...");
+        System.out.println("Aman is present");
 
 
         System.out.println("Harry Grade 11 is present!");
