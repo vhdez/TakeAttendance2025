915ea284cbf5c4ad068f29896ea400be85c358b2
amaneasterling <aeasterling26@scienceleadership.org>
2025-09-08 13:36:38 -0400

    Finally Aman can push!!!

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,46 @@ public class Main {
 
         System.out.println("Good morning!");
         System.out.println("Let's take attendance...");
-        System.out.println("Mr. Hernandez is present!");
 
-        /*
+
+        System.out.println("Harry Grade 11 is present!");
+        System.out.println("Minghao Grade  12 is maybe present!");
+        System.out.println("Alex is present");
+        System.out.println("Sam is present");
+        System.out.println("Tai is present");
+        System.out.println("Adil is present.");
+
+        Teacher mrH = new Teacher();
+        mrH.name = "Mr. Hernandez";
+        mrH.present = false;
+        mrH.password = 9884;
+        mrH.greeting();
+
+        // 1. Create an Object for yourself
+        // 2. Store YOUR values in YOUR object
+        // 3. Tell YOUR object to announce themselves
+
         Scanner myTextScanner = new Scanner(System.in);  // Create a Scanner object
         boolean notDoneYet = true;
-        while (true) {
-            System.out.print("Taking attendance.  Type your name: OR Teacher, type DONE");
-            String nameTyped = myTextScanner.nextLine();  // Read user input
-            if (nameTyped.equals("DONE")) {
-                // STOP THIS LOOP
-                notDoneYet = false;
-            } else {
-                System.out.println(nameTyped + " is present!");
-            }
-        }
+        while (notDoneYet) {
+            System.out.print("Type your name:");
+            String typedName = myTextScanner.nextLine();  // Read user input
+            System.out.print("Type your password:");
+            String typedPassword = myTextScanner.nextLine();  // Read user input
 
-         */
+            // typedPassword = "9884"
+            // BUT
+            // mrH.password = 9884 (the integer)
+            int typedPasswordNumber = Integer.valueOf(typedPassword);
+            if (mrH.name.equals(typedName)) {
+                if (mrH.password == typedPasswordNumber) {
+                    mrH.present = true;
+                }
+            }
+            mrH.status();
+
+            // 4. Get typing input to check for your name/password/presence
+
+        }
     }
 }
